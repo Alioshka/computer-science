692072cbd86bfd989f5289288b7a05e2797c8543
Aliaksei Alioshka <Aliaksei_Alioshka@epam.com>
2019-02-12 15:04:44 -0500

    added example of anagrams

---
+++ challenges/anagrams/test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const src = require('./index');
+
+Object.entries(src).forEach(entry => {
+    let fnName, fn;
+    [fnName, fn] = entry;
+
+    describe(`Given ${fnName}('rail safety', 'fairy tales')`, () => {
+        test(`return true`, () => {
+            expect(fn('rail safety', 'fairy tales')).toEqual(true);
+        });
+    });
+
+    describe(`Given ${fnName}('RAIL! SAFETY!', 'fairy tales')`, () => {
+        test(`${fnName} return true`, () => {
+            expect(fn('RAIL! SAFETY!', 'fairy tales')).toEqual(true);
+        });
+    });
+
+    describe(`Given ${fnName}('Hi there', 'Bye there')`, () => {
+        test(`${fnName} return false`, () => {
+            expect(fn('Hi there', 'Bye there')).toEqual(false);
+        });
+    });
+});
