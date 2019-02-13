6d3f8f499e98fe7a95cdff2541b7148cba12cdd8
Aliaksei Alioshka <Aliaksei_Alioshka@epam.com>
2019-02-13 18:53:32 -0500

    capitalize solution

---
+++ challenges/capitalize/test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const src = require('./index');
+
+const testData = {
+    'hi there, how is it going?': 'Hi There, How Is It Going?',
+    'i love breakfast at bill miller bbq': 'I Love Breakfast At Bill Miller Bbq'
+};
+
+Object.entries(src).forEach(entry => {
+    let fnName, fn;
+    [fnName, fn] = entry;
+
+    Object.keys(testData).forEach(key => {
+        test(`${fnName}("${key}") should return "${testData[key]}"`, () => {
+            expect(fn(key)).toEqual(testData[key]);
+        });
+    });
+});
