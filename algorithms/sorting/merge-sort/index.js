27fec82be7469a90ad2cc701ab7c978903bedf16
Aliaksei Alioshka <Aliaksei_Alioshka@epam.com>
2019-06-07 16:53:55 -0400

    added merge sort example with explanations removed bad merge sort example with shift and concat

---
+++ algorithms/sorting/merge-sort/index.js
@@ -0,0 +1,40 @@
+// Merges two already sorted arrays
+function merge(arr1, arr2){
+    let results = [];
+    let i = 0;
+    let j = 0;
+    while(i < arr1.length && j < arr2.length){
+        if(arr2[j] > arr1[i]){
+            results.push(arr1[i]);
+            i++;
+        } else { // case if arr2[j] less or equal
+            results.push(arr2[j]);
+            j++;
+        }
+    }
+    // as arrays length can be not equal,
+    // but we for sure know that arr1 and arr2 are sorted
+    // we simply add what's left at the end
+    while(i < arr1.length) {
+        results.push(arr1[i]);
+        i++;
+    }
+    while(j < arr2.length) {
+        results.push(arr2[j]);
+        j++;
+    }
+    return results;
+}
+
+// Recursive Merge Sort
+function mergeSort(arr){
+    if(arr.length <= 1) return arr;
+    let mid = Math.floor(arr.length/2);
+    let left = mergeSort(arr.slice(0,mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left, right);
+}
+
+module.exports = {
+    mergeSort
+};
