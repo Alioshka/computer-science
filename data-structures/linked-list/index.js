2bdf11161432afa0a82a1979edb17755ffadaa88
Aliaksei Alioshka <Aliaksei_Alioshka@epam.com>
2019-09-03 10:51:42 -0400

    added "pop" and "shift" implementation for singly linked list

--- data-structures/linked-list/index.js
+++ data-structures/linked-list/index.js
@@ -33,6 +33,36 @@ class SinglyLinkedList{
         this.length++;
         return this;
     }
+
+    pop(){
+        if(!this.head) return undefined;
+        var current = this.head;
+        var newTail = current;
+        while(current.next){
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+
+    }
+
+    shift(){
+        if(!this.head) return undefined;
+        var currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if(this.length === 0){
+            this.tail = null;
+        }
+        return currentHead;
+    }
 }
 
 module.exports = {
--- data-structures/linked-list/test.js
+++ data-structures/linked-list/test.js
@@ -21,4 +21,34 @@ describe(`SinglyLinkedList`, () => {
         expect(list.head.next.next.val).toEqual('GOODBYE');
         expect(list.head.next.next.next.val).toEqual('!');
     });
+
+    test(`should pop data from the list`, () => {
+        list.push('HELLO');
+        list.push("!");
+        expect(list.head.val).toEqual('HELLO');
+        expect(list.tail.val).toEqual('!');
+        expect(list.tail.next).toEqual(null);
+        expect(list.head.next.val).toEqual('!');
+
+        let oldTail = list.pop();
+        expect(oldTail.val).toEqual('!');
+
+        expect(list.head.val).toEqual('HELLO');
+        expect(list.head.next).toEqual(null);
+        expect(list.tail.val).toEqual('HELLO');
+    });
+
+    test(`should shift data from the list`, () => {
+        list.push('HELLO');
+        list.push('Alex');
+        list.push('!');
+
+
+        let oldHead = list.shift();
+        expect(oldHead.val).toEqual('HELLO');
+
+        expect(list.head.val).toEqual('Alex');
+        expect(list.head.next.val).toEqual('!');
+        expect(list.tail.val).toEqual('!');
+    });
 });
