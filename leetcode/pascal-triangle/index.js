1dc08b1fcad491356bb81401dd8641f1858b417f
Aliaksei Alioshka <Aliaksei_Alioshka@epam.com>
2019-10-29 10:45:06 -0400

    leetcode Pascal's Triangle solution

---
+++ leetcode/pascal-triangle/index.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    let result = [];
+    if(numRows <= 0) {
+        return result;
+    }
+
+    // fill row #0
+    result[0] = [1];
+
+    for(let i = 1; i < numRows; i++){
+        // first element in a row is always 1
+        result[i] = [1];
+
+        // this loop never happends for row #1
+        for(let j = 1; j < i; j++){
+            result[i].push(result[i-1][j-1] + result[i-1][j])
+        }
+
+        // last element in a row is always 1
+        result[i].push(1);
+    }
+
+    return result;
+};
